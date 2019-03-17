0e22b5618ce17d69773f2b89b31d35609da5fee4
PC Nayara <wowluisjnr@gmail.com>
2019-03-17 14:51:51 -0300

    Corrigindo local do contentHeader

--- frontend/src/components/templates/Main.jsx
+++ frontend/src/components/templates/Main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ContentHeader from './ContentHeader';
 
 export default props => 
-    <React.Fragment>
-        <ContentHeader />
+    <React.Fragment>        
         <div className="content-wrapper"> 
+            <ContentHeader />
             <section className="content">           
                 {props.children}   
             </section>        
